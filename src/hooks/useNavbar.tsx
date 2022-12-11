e0bf180c87ba5c80e017b1c4ada70361aea7f364
taehee <jangth0655@gmail.com>
2022-12-11 11:54:57 +0900

    [refact] useNavbar navigate 클래스를 활용해 로직 간소화

--- src/hooks/useNavbar.tsx
+++ src/hooks/useNavbar.tsx
@@ -1,28 +1,16 @@
 import { useNavigate } from 'react-router-dom';
-import { currentPage } from '../model/types';
 import navbar from '../utils/navbarMenu';
 
 const useNavbar = () => {
   const navigate = useNavigate();
 
   const onPage = (pageName: string) => {
-    switch (pageName) {
-      case '홈':
-        navigate(currentPage.HOME);
-        break;
-      case '영화':
-        navigate(currentPage.MOVIE);
-        break;
-      case 'TV':
-        navigate(currentPage.TV);
-        break;
-      case '검색':
-        navigate(currentPage.SEARCH);
-    }
+    const name = navbar.getPageName(pageName);
+    navigate(name);
   };
 
   return {
-    navbar: navbar.NavbarItems,
+    navbar: navbar.getNavbarItems,
     onPage,
   };
 };
--- src/utils/navbarMenu.ts
+++ src/utils/navbarMenu.ts
@@ -1,12 +1,27 @@
 import { navbarData } from '../data/data';
-import { Navbar } from '../model/types';
+import { currentPage, Navbar } from '../model/types';
 
 class NavbarMenu {
   constructor(private navbarItems: Navbar[]) {}
 
-  get NavbarItems() {
+  get getNavbarItems() {
     return [...this.navbarItems];
   }
+
+  getPageName = (pageName: string) => {
+    switch (pageName) {
+      case '홈':
+        return currentPage.HOME;
+      case '영화':
+        return currentPage.MOVIE;
+      case 'TV':
+        return currentPage.TV;
+      case '검색':
+        return currentPage.SEARCH;
+      default:
+        return currentPage.HOME;
+    }
+  };
 }
 
 const navbar = new NavbarMenu(navbarData);
